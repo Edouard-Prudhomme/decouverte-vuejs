3a283d9b4e8f2b6edfd235cb2ec2fc12300f7b46
Edouard-Prudhomme <edo.prudhomme@yahoo.fr>
2022-05-19 16:04:17 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1> Hello {{ name }}!</h1>
+    <h2 v-if="isUserConnected">Vous êtes connecté</h2>
+    <h2 v-else>Veuillez vous connecter</h2>
+    <h3 v-show="isUserConnected">Content de vous revoir !</h3>
+
+
+    <!-- <div>{{ contenuHTML }}</div> -->
+    <div v-html="contenuHTML"></div>
+
+    <form action="" @submit.prevent="onFormSubmit">
+      <!-- <input type="text" name="machin" id="machin" v-bind:value="nomPromo"> -->
+      <input type="text" name="machin" id="machin" v-model="nomPromo">
+      <button role="submit">Valider</button>
+    </form>
+
+    <ul>
+      <!-- <li v-for="fruit in fruitList">{{ fruit }}</li> -->
+      <li v-for="fruit in fruitList" v-bind:key="fruit.id">{{ fruit.name }}</li>
+    </ul>
+
+
+    <CustomFooter :today="currentDay"></CustomFooter>
+  </div>
+
+</template>
+
+<script>
+import CustomFooter from './components/CustomFooter.vue';
+
+  export default {
+    data() {
+      return {
+        name: "Vue",
+        contenuHTML: "<strong>Bienvenous à Vue</strong>",
+        nomPromo : "",
+        fruitList: [
+          {id: 1, name: "pommes"},
+          {id: 2, name: "poires"},
+          {id: 3, name: "fraises"},
+          {id: 4, name: "raisins"}
+        ],
+        isUserConnected: false,
+        currentDay: "18/05/2022"
+      };
+    },
+    methods: {
+      onFormSubmit(event) {
+        console.log("Le nom de la promo est " + this.nomPromo);
+      }
+    },
+    components: {
+      CustomFooter
+    }
+  };
+</script>
---
+++ src/components/CustomFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+    <a href="/mentions-legales">Mentions légales</a>
+    <a href="/contact">Contact</a>
+    {{ today }}
+  </footer>    
+</template>
+
+<script>
+export default {
+  props: ["today"]
+}
+</script>
+
+<style lang="scss">
+.footer {
+  background-color: black;  
+  padding: 1rem;
+  color: white;
+
+  a {
+    color: white;
+  }
+}
+</style>
